59dbc67ea4825969794013df91b3f680cf0aefc1
Brian-big <briannbig@gmail.com>
2021-09-18 12:42:17 +0300

    add login screen

---
+++ app/src/main/java/app/smartattend/MainActivity.kt
@@ -0,0 +1,14 @@
+package app.smartattend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.smartattend.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
