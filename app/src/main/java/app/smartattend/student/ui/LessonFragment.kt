d93cf9f3aeba26b4bdd03942a7d461bc54d9f7e9
Brian-big <briannbig@gmail.com>
2021-09-19 19:53:01 +0300

    share qr code

--- app/src/main/java/app/smartattend/student/ui/LessonFragment.kt
+++ app/src/main/java/app/smartattend/student/ui/LessonFragment.kt
@@ -1,23 +1,72 @@
 package app.smartattend.student.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import app.smartattend.R
+import app.smartattend.commons.LessonViewModel
 import app.smartattend.databinding.FragmentLessonBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
 class LessonFragment : Fragment() {
 
 
     private lateinit var binding: FragmentLessonBinding
+    private lateinit var lessonViewModel: LessonViewModel
+    private lateinit var btmSheet: View
+    private lateinit var ivQRCode: ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLessonBinding.inflate(inflater, container, false)
+        lessonViewModel = ViewModelProvider(this).get(LessonViewModel::class.java)
+        btmSheet = binding.root.findViewById(R.id.layout_bottomSheetBehaviorShare)
+        ivQRCode = binding.root.findViewById(R.id.ivDisplayQR)
+        val bottomSheetBehavior = BottomSheetBehavior.from(btmSheet)
+        binding.fabShareCode.setOnClickListener {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED){
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                binding.fabShareCode.visibility = View.GONE
+                setQRImage()
+            }
+            else bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    binding.fabShareCode.visibility = View.GONE
+                    setQRImage()
+                }
+                else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    binding.fabShareCode.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
         return binding.root
     }
-    
+    private fun setQRImage() {
+       val url = Gson().toJson(lessonViewModel.getLesson().value)
+        try {
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.encodeBitmap(url, BarcodeFormat.QR_CODE, 400, 400)
+            ivQRCode.setImageBitmap(bitmap)
+        }catch (e: Exception){}
+
+    }
+
+    fun snack(message: String, length: Int = Snackbar.LENGTH_SHORT){
+        Snackbar.make(requireView().rootView, message, length).show()
+    }
 }
