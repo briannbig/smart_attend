a01b46a0746ac0e9b57314eb66ed2f0faa797a39
Brian-big <briannbig@gmail.com>
2021-09-20 16:03:44 +0300

    login success

--- app/src/main/java/app/smartattend/firebase/FirebaseDB.kt
+++ app/src/main/java/app/smartattend/firebase/FirebaseDB.kt
@@ -1,6 +1,5 @@
 package app.smartattend.firebase
 
-import app.smartattend.model.User
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -10,13 +9,6 @@ object FirebaseDB {
     private val lessonRef = database.getReference("Lessons")
     private val courseRef = database.getReference("Courses")
     private val studentRef = database.getReference("Students")
-    private val userRef = database.getReference("Users")
+    val userRef = database.getReference("Users")
     private val classRef = database.getReference("Classes")
-
-    fun insertUser(user: User): Boolean {
-        return true
-    }
-    fun getUser(): User{
-        return User()
-    }
 }
--- app/src/main/java/app/smartattend/model/User.java
+++ app/src/main/java/app/smartattend/model/User.java
@@ -1,7 +1,7 @@
 package app.smartattend.model;
 
 public class User {
-    private String username, password;
+    private String username, password, userType;
 
     public User() {
     }
@@ -9,6 +9,12 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public User(String username, String password, String userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
     }
 
     public String getUsername() {
@@ -25,5 +31,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
     }
 }
