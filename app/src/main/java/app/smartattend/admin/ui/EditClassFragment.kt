a26883c1057b02cc37ac67e856e16ffbc4dc962d
Brian-big <briannbig@gmail.com>
2021-09-21 13:45:01 +0300

    manage classes

--- app/src/main/java/app/smartattend/admin/ui/EditClassFragment.kt
+++ app/src/main/java/app/smartattend/admin/ui/EditClassFragment.kt
@@ -1,12 +1,17 @@
 package app.smartattend.admin.ui
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import app.smartattend.R
 import app.smartattend.databinding.FragmentEditClassBinding
+import app.smartattend.firebase.FirebaseDB
+import app.smartattend.model.Class
+import com.google.android.material.snackbar.Snackbar
 
 class EditClassFragment : Fragment() {
 
@@ -17,7 +22,20 @@ class EditClassFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentEditClassBinding.inflate(inflater, container, false)
+        binding.button.setOnClickListener {
+            saveClass()
+        }
         return binding.root
+    }
+
+    private fun saveClass() {
+        if (!TextUtils.isEmpty(binding.etClassId.text) &&
+            !TextUtils.isEmpty(binding.etProgram.text)){
+            FirebaseDB.classRef.child(binding.etClassId.text.toString())
+                .setValue(Class(binding.etClassId.text.toString(), binding.etProgram.text.toString()))
+            findNavController().navigateUp()
+        }
+        else Snackbar.make(requireView(), "All fields are required", Snackbar.LENGTH_SHORT).show()
     }
 
     companion object
--- app/src/main/java/app/smartattend/firebase/FirebaseDB.kt
+++ app/src/main/java/app/smartattend/firebase/FirebaseDB.kt
@@ -5,10 +5,10 @@ import com.google.firebase.ktx.Firebase
 
 object FirebaseDB {
     private var database = Firebase.database.apply { setPersistenceEnabled(true) }
-    private val lecturerRef = database.getReference("Lecturers")
+    val lecturerRef = database.getReference("Lecturers")
     private val lessonRef = database.getReference("Lessons")
-    private val courseRef = database.getReference("Courses")
+    val courseRef = database.getReference("Courses")
     private val studentRef = database.getReference("Students")
     val userRef = database.getReference("Users")
-    private val classRef = database.getReference("Classes")
+    val classRef = database.getReference("Classes")
 }
