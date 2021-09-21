6e26e0da76b1e1661059c33985c138223c9c6e10
Brian-big <briannbig@gmail.com>
2021-09-21 15:19:01 +0300

    logout

--- app/src/main/java/app/smartattend/admin/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/app/smartattend/admin/ui/notifications/NotificationsFragment.kt
@@ -1,45 +1,55 @@
 package app.smartattend.admin.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import app.smartattend.MainActivity
 import app.smartattend.R
 import app.smartattend.databinding.FragmentNotificationsBinding
+import app.smartattend.preferences.AppPreferences
+import com.google.firebase.auth.FirebaseAuth
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentNotificationsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    ): View {
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        binding.cvLogout.setOnClickListener { logout() }
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun logout() {
+        val builder: AlertDialog.Builder? = activity?.let {
+            AlertDialog.Builder(it)
+        }
+        builder?.apply {
+            setTitle("You are about to log out")
+            setMessage("Proceed?")
+            setPositiveButton("Yes") { _, _ ->
+                AppPreferences(requireContext()).preferences.edit().clear().apply()
+                activity?.apply {
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                    finish()
+                }
+            }
+            setNegativeButton("No"){_, _ -> }
+        }
+        builder?.setMessage("Proceed?")
+            ?.setTitle("You are about to log out")
+        val dialog: AlertDialog? = builder?.create()
+        dialog?.show()
     }
 }
