94a9d04929e624119bb5a5aafcad244c4ee52259
Brian-big <briannbig@gmail.com>
2021-09-26 13:11:01 +0300

    analyze for a course

--- app/src/main/java/app/smartattend/reports/ReportUtil.kt
+++ app/src/main/java/app/smartattend/reports/ReportUtil.kt
@@ -1,10 +1,26 @@
 package app.smartattend.reports
 
+import app.smartattend.firebase.FirebaseDB
+import app.smartattend.model.Course
 import com.google.firebase.database.DataSnapshot
 
 class ReportUtil {
 
-    fun analyzeStudPercentage(snapshot: DataSnapshot, reg_no: String): Int {
+    private var courseReport = mutableMapOf<String, Int>()
+    fun analyzeForSpecificCourse(course: Course): MutableMap<String, Int> {
+        val lessonSnapshot = FirebaseDB.lessonRef.orderByChild("course").equalTo(course.code).get()
+        val studentsSnapshot = FirebaseDB.studentRef.orderByChild("classId").equalTo(course.classId).get()
+        lessonSnapshot.addOnSuccessListener {
+            val lesSnapshot = it
+            studentsSnapshot.addOnSuccessListener {
+                for (i in it.children){
+                    analyzeStudPercentage(lesSnapshot, i.child("reg_no").value.toString())
+                }
+            }
+        }
+        return courseReport
+    }
+    private fun analyzeStudPercentage(snapshot: DataSnapshot, reg_no: String){
         var sum = 0; var count = 0
         for (i in snapshot.children){
             if (i.hasChild(reg_no)){
@@ -12,6 +28,7 @@ class ReportUtil {
             }
             count ++
         }
-        return (sum/count) * 100
+        val avg = (sum/count) * 100
+        courseReport[reg_no] = avg
     }
 }
