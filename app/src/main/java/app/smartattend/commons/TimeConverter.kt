426ebc82b11447fb4e989baedc6f333d26f80fd1
brianBig <briannbig@gmail.com>
2022-04-02 16:02:40 +0300

    Add TimeConverter.kt

---
+++ app/src/main/java/app/smartattend/commons/TimeConverter.kt
@@ -0,0 +1,16 @@
+package app.smartattend.commons
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+//  this utility class facilitates changing time stored in our database in Long primitive type
+// to more human understandable format.
+// formats the given Long to dd/MM/yyyy HH:mm
+class TimeConverter {
+    fun toDateFormat(timeInMillis: Long): String {
+        val dateFormat: DateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US)
+        val date = Date(timeInMillis)
+        return dateFormat.format(date)
+    }
+}
